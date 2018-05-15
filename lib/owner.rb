4e92eb4b300a59bff7de868019a9d8b6749e65e1
Wasif Khan <waasif.khaan@gmail.com>
2018-05-15 21:01:22 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,24 @@
 class Owner
-  # code goes here
+  attr_accessor :name 
+  @@all = []
+  @@counter = 0
+ def initialize(name)
+   @name = name 
+   @@all << self
+   @@counter  =+ 1
+ end 
+ 
+ def self.all 
+   @@all 
+ end 
+# def self.count 
+#   @@counter
+# end 
+ def self.reset_all
+   @@all.clear
+ end 
+ def self.count 
+   @@counter = 0 
+ end 
+   
 end
